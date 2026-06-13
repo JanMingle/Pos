489787da42d57878b3cf23c72525b6197914bb6f
JanMingle <jan.mongwe@gmail.com>
2026-06-13 14:31:00 +0200

    Improve JMingle onboarding and POS sales flow

---
+++ PosPlatform.Infrastructure/Migrations/20260613103707_AddBusinessSetupCompleted.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PosPlatform.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddBusinessSetupCompleted : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
